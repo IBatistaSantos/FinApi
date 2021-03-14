b606fe49299e4881f2c7785f91e66735e5a1c5a8
Israel Batista <israelbatiista19@gmail.com>
2021-03-14 20:56:53 -0300

    Identação do código

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 const express = require('express');
-const {v4: uuidV4} = require('uuid')
-
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
 
 const customers = [];
 
-app.post("/account", (request, response) => {
+app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
   const customerAlreadyExists = customers.some(
-    (customer) => customer.cpf = cpf);
+    (customer) => (customer.cpf = cpf)
+  );
 
-    if (customerAlreadyExists) {
-      return response
-      .status(400)
-      .json({error : "Customer already Exsits!"})
-    }
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already Exsits!' });
+  }
 
-    customers.push({
+  customers.push({
     cpf,
     name,
     id: uuidV4(),
-    statement: []
-  })
+    statement: [],
+  });
 
   return response.status(201).send();
-})
+});
 
-app.listen(3333)
+app.listen(3333);
