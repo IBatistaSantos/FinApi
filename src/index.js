e82f83759d9545b0111e96d95b7e8ab14561676c
Israel Batista <israelbatiista19@gmail.com>
2021-03-14 20:40:00 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const {v4: uuidV4} = require('uuid')
+
+
+const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf = cpf);
+
+    if (customerAlreadyExists) {
+      return response
+      .status(400)
+      .json({error : "Customer already Exsits!"})
+    }
+
+    customers.push({
+    cpf,
+    name,
+    id: uuidV4(),
+    statement: []
+  })
+
+  return response.status(201).send();
+})
+
+app.listen(3333)
