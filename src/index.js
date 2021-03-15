cc510b0d2bc29d81085f9a27463f084f24f3182e
Israel Batista <israelbatiista19@gmail.com>
2021-03-15 12:40:44 -0300

    Middleware verfyIfExists account

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccount(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf == cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
   const customerAlreadyExists = customers.some(
@@ -26,15 +40,9 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf == cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' });
-  }
-
+app.get('/statement', verifyIfExistsAccount, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
+
 app.listen(3333);
