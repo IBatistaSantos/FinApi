aafce62c4f850b0ca160422395ef67575e2eddc8
Israel Batista <israelbatiista19@gmail.com>
2021-03-15 13:07:22 -0300

    Developed credit feature

--- src/index.js
+++ src/index.js
@@ -20,6 +20,18 @@ function verifyIfExistsAccount(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
   const customerAlreadyExists = customers.some(
@@ -43,6 +55,44 @@ app.post('/account', (request, response) => {
 app.get('/statement', verifyIfExistsAccount, (request, response) => {
   const { customer } = request;
   return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccount, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccount, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds!' });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
